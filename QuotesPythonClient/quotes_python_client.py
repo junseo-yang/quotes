afe7cd821b220ded3697f13de7017fffe54bd2ce
junseo-yang <jsy724724@gmail.com>
2023-12-05 10:39:12 -0500

    Implemented JWT Auth in QuotesPythonClient

--- QuotesPythonClient/quotes_python_client.py
+++ QuotesPythonClient/quotes_python_client.py
@@ -1,3 +1,10 @@
+# quotes_python_client.py
+#   PROG3170 Assignment 
+# 
+# Revision History
+#   Junseo Yang 2023-11-05 Created
+#   Junseo Yang 2023-12-10 Updated
+
 import os
 import requests
 import urllib3
@@ -10,14 +17,22 @@ TEXT_FILE = ROOT_DIR / 'quotes.txt'
 # Suppress the warning when http request to localhost 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# Clear Console
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 def load_quotes():
+    # Validate Access Token 
+    if access_token == "":
+        print()
+        print("You should get access token to use the option.")
+        return
+    
     url = 'https://localhost:7223/api/quotes'
 
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token,
     }
 
     json_data_list = []
@@ -37,16 +52,25 @@ def load_quotes():
     try:
         for json in json_data_list:
             requests.post(url, json=json, headers=headers, verify=False)
-        
+
+        print()
         print("Quotes have been loaded successfully.")
     except:
+        print()
         print("Something went wrong. Check the API Server.")
 
 def display_random_quote():
+    # Validate Access Token 
+    if access_token == "":
+        print()
+        print("You should get access token to use the option.")
+        return
+    
     url = 'https://localhost:7223/api/quotes'
 
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token,
     }
 
     try:
@@ -62,14 +86,22 @@ def display_random_quote():
         print("A random quote")
         print(f"'{quote['description']}' by '{quote['author']}' (Likes: {quote['like']})")
     except:
+        print()
         print("Something went wrong. Check the API Server.")
 
 def add_new_quote():
+    # Validate Access Token
+    if access_token == "":
+        print()
+        print("You should get access token to use the option.")
+        return
+
     quotes_url = 'https://localhost:7223/api/quotes'
     tags_url = 'https://localhost:7223/api/tags'
 
     headers = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token,
     }
 
     try:
@@ -82,22 +114,133 @@ def add_new_quote():
 
         # Get User Input to create quote
         print()
-        quote_input = input("Enter a quote: ")
-        author_input = input("Enter an author: ")
-        tags_input = [tag for tag in input("Enter tags from Available Tags (Use ', ' for separate. e.g., 'life, happiness'): ").split(", ")]
+        quote_input = input("Enter a quote: ").strip()
+        author_input = input("Enter an author: ").strip()
+        tags_input = [tag for tag in input("Enter tags from Available Tags (Use ', ' for separate. e.g., 'life, happiness'): ").strip().split(", ")]
         json = {
             "description": quote_input,
             "author": author_input,
             "tags": tags_input}
         
-        # Post
+        # Post to add a new quote
         response = requests.post(quotes_url, json=json, headers=headers, verify=False)
         if (response.ok):
+            print()
             print("The quote has been created successfully.")
         else:
             raise Exception('Network Error')
     except:
+        print()
         print("Something went wrong. Check the API Server. Or enter valid tags.")
+
+def register_user():
+    roles_url = 'https://localhost:7223/api/roles'
+    register_url = 'https://localhost:7223/api/register'
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        resp = requests.get(roles_url, headers=headers, verify=False)
+
+        if resp.status_code != 200:
+            print()
+            print("Error: Check the API first.")
+        else:
+            print()
+            print("Registration")
+            first_name = input('Enter a first name: ').strip()
+            last_name = input('Enter a last name: ').strip()
+            username = input('Enter a username (Required): ').strip()
+            password = input('Enter a password (Required): ').strip()
+            email = input('Enter an email (Required): ').strip()
+            phone_number = input('Enter a phone number: ').strip()
+
+            available_roles = resp.json()["roles"]
+            print(f"Available Roles: {available_roles}")
+            roles = input('Enter roles (Comma Separated. E.g., QUOTE_USER, QUOTE_MANAGER): ').strip()
+
+            error_message = ""
+
+            # Input Validation
+            if (username == ""):
+                error_message += "\nError: username is required."
+            if (password == ""):
+                error_message += "\nError: password is required."
+            if (email == ""):
+                error_message += "\nError: email is required."
+            
+            if error_message:
+                print(error_message)
+            else:
+                register_request = {
+                    'username': username,
+                    'password': password,
+                    'email': email
+                }
+
+                if (first_name):
+                    register_request["firstName"] = first_name
+                if (last_name):
+                    register_request["lastName"] = last_name
+                if (phone_number):
+                    register_request["phoneNumber"] = phone_number
+                if (roles):
+                    register_request["roles"] = roles.split(", ")
+
+                try:
+                    resp = requests.post(register_url, headers=headers, json=register_request, verify=False)
+
+                    if resp.status_code == 201:
+                        print()
+                        print("User registered successfully.")
+                    else:
+                        results = resp.json()
+                        print()
+                        print("Errors:")
+                        for k, v in results.items():
+                            print(f"{k}: {v}")
+                except:
+                    print()
+                    print("Error: Check the API")
+    except:
+        print()
+        print("Error: Check the API or Enter a valid roles.")
+
+def login_user():
+    login_url = 'https://localhost:7223/api/login'
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        # User Input
+        print()
+        print("Login")
+        username = input('Enter a username: ').strip()
+        password = input('Enter a password: ').strip()
+            
+        login_request = {
+            'userName': username,
+            'password': password
+        }
+
+        resp = requests.post(login_url, headers=headers, json=login_request, verify=False)
+        login_result = resp.json()
+
+        if resp.status_code == 200:
+            global access_token 
+            access_token = login_result["token"]
+            print()
+            print("User logged in successfully.")
+        else:
+            print()
+            print("Error: Check your email and password again")
+    except:
+        print()
+        print("Error: Check the API")
 
 # Clear Console
 cls()
@@ -105,14 +248,20 @@ print("Welcome to QuotesPythonClient!")
 
 quotes_loaded = False
 
+# Store access_token
+access_token = ""
+
 while True:
     print()
     print("Options")
-    print("1. Add a new quote")
-    print("2. Display a randomly selected quote")
+    print("1. Register a user")
+    print("2. Login")
+    print("3. Add a new quote (Login Required)")
+    print("4. Display a randomly selected quote (Login Required)")
     if not quotes_loaded:
-        print("3. Load quotes to the Web API (One time only)")
-    user_input = input("Select an option by input number only:")
+        print("5. Load quotes to the Web API (One time only) (Login Required)")
+    print("6. Exit")
+    user_input = input("Select an option by input number only:").strip()
 
     # Validation
     selection = 0
@@ -124,14 +273,21 @@ while True:
     
     # Execute
     if selection == 1:
-        add_new_quote()
+        register_user()
     elif selection == 2:
-        display_random_quote()
+        login_user()
     elif selection == 3:
+        add_new_quote()
+    elif selection == 4:
+        display_random_quote()
+    elif selection == 5:
         if quotes_loaded:
             print("Quotes has already been loaded.")
         else:
             quotes_loaded = True
             load_quotes()
+    elif selection == 6:
+        print("Thank you. Bye!")
+        break
     else:
-        print("Select option 1 ~ 3. Try Again.")
+        print("Select option 1 ~ 6. Try Again.")
